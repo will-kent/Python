b11080c21833afe4c8048bff78c4b4935bf6f107
will-kent <wjokent@gmail.com>
2020-04-10 18:17:54 +1000

    AliensAttack Game

---
+++ Games/AliensAttack/Aliens.py
@@ -0,0 +1,162 @@
+import pygame, sys, random, math
+import pygame.locals as GAME_GLOBALS
+import pygame.event as GAME_EVENTS
+import pygame.time as GAME_TIME
+import Ships
+
+windowWidth = 1024
+windowHeight = 614
+
+pygame.init()
+pygame.font.init()
+surface = pygame.display.set_mode((windowWidth, windowHeight))
+
+pygame.display.set_caption('Space Invaders Are Gonna Kill Me!')
+textFont = pygame.font.SysFont("monospace", 50)
+scoreFont = pygame.font.SysFont("monospace", 20)
+
+gameStarted = False
+gameStartedTime = 0
+gameFinishedTime = 0
+gameOver = False
+playerName = "default"
+enemyShips = []
+score = 0
+
+# Mouse variables
+mousePosition = (0,0)
+mouseStates = None
+mouseDown = False
+
+# Image variables
+startScreen = pygame.image.load("assets/start_screen_new.png")
+background = pygame.image.load("assets/background.png")
+backgroundLost = pygame.image.load("assets/background_end.png")
+
+#Colour variables
+white = pygame.Color(255,255,255)
+
+lastEnemyCreated = 0
+enemyInterval = random.randint(1000, 2500)
+
+# Sound setup
+pygame.mixer.init()
+
+def updateGame(score):
+    
+    global mouseDown, gameOver
+
+    enemyDead = score
+    
+    if mouseStates[0] is 1 and mouseDown is False:
+        ship.fire()
+        mouseDown = True
+    elif mouseStates[0] is 0 and mouseDown is True:
+        mouseDown = False
+
+    ship.setPosition(mousePosition)
+
+    enemiesToRemove = []
+
+    for idx, enemy in enumerate(enemyShips):
+
+        if enemy.y < windowHeight:
+            enemy.move()
+            enemy.tryToFire()
+            shipIsDestroyed = enemy.checkForHit(ship)
+            enemyIsDestroyed = ship.checkForHit(enemy)
+
+            if enemyIsDestroyed is True:
+                enemyDead = enemyDead + 1
+                enemiesToRemove.append(idx)
+
+            if shipIsDestroyed is True:
+                gameOver = True
+
+        else:
+            enemiesToRemove.append(idx)
+
+    for idx in enemiesToRemove:
+        del enemyShips[idx]
+
+    return enemyDead
+        
+def drawGame(playerName):
+    surface.blit(background, (0,0))
+    ship.draw(playerName)
+    ship.drawBullets()
+
+    for enemy in enemyShips:
+        enemy.draw(playerName)
+        enemy.drawBullets()
+
+def playerLost(playerName):
+    surface.blit(backgroundLost, (0,0))
+    lostText = textFont.render("Space Pilot " + playerName + " lost", 1, white)
+    lostRect = lostText.get_rect()
+    lostRect.center = (windowWidth / 2, windowHeight / 2)
+    surface.blit(lostText, lostRect)
+
+def quitGame():
+    pygame.quit()
+    sys.exit()
+
+def setScore(score, playerName):
+    scoreWords = "Space Pilot " + playerName + " has killed " + str(score) + " aliens"
+    scoreText = scoreFont.render(scoreWords, 1, white)
+    scoreRect = scoreText.get_rect()
+    scoreRect.center = (windowWidth - (len(scoreWords) * 7), 10)
+    surface.blit(scoreText, scoreRect)
+
+# 'main' loop
+while True:
+
+    timeTick = GAME_TIME.get_ticks()
+    mousePosition = pygame.mouse.get_pos()
+    mouseStates = pygame.mouse.get_pressed()
+
+    if gameStarted is True and gameOver is False:
+
+        score = updateGame(score)
+        drawGame(playerName)
+        setScore(score, playerName)
+
+    elif gameStarted is False and gameOver is False:
+        surface.blit(startScreen, (0,0))
+        
+        if mouseStates[0] is 1:
+            if mousePosition[0] > 210 and mousePosition[0] < 475 and mousePosition[1] > 400 and mousePosition[1] < 490:
+
+                playerName = "James"
+                gameStarted = True
+                ship = Ships.Player(windowWidth / 2, windowHeight, pygame, surface, playerName)
+
+            elif mousePosition[0] > 575 and mousePosition[0] < 810 and mousePosition[1] > 400 and mousePosition[1] < 510:
+
+                playerName = "Florence"
+                gameStarted = True
+                ship = Ships.Player(windowWidth / 2, windowHeight, pygame, surface, playerName)
+ 
+        elif mouseStates[0] is 0 and mouseDown is True:
+            mouseDown = False
+
+    elif gameStarted is True and gameOver is True:
+        playerLost(playerName)
+            
+    #Handle user and system events
+    for event in GAME_EVENTS.get():
+
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_ESCAPE:
+                quitGame()
+
+    if GAME_TIME.get_ticks()-lastEnemyCreated > enemyInterval and gameStarted is True:
+
+        enemyShips.append(Ships.Enemy(random.randint(0, windowWidth), -60, pygame, surface, 1))
+        lastEnemyCreated = GAME_TIME.get_ticks()
+
+    if event.type == GAME_GLOBALS.QUIT:
+        quitGame()
+
+    pygame.display.update()
---
+++ Games/AliensAttack/Ships.py
@@ -0,0 +1,116 @@
+import Projectiles, random
+
+class Player():
+
+    x = 0
+    y = 0
+    firing = False
+    image = None
+    soundEffect = "sounds/player_gun.wav"
+    pygame = None
+    surface = None
+    width = 0
+    height = 0
+    bullets = []
+    bulletImage = "assets/you_pellet.png"
+    bulletSpeed = -10
+    health = 5
+
+    def loadImages(self, playerName):
+        if playerName == "James":
+            self.image = self.pygame.image.load("assets/james_ship_v2.png")
+        elif playerName == "Florence":
+            self.image = self.pygame.image.load("assets/florence_ship_v2.png")
+        else:
+            self.image = self.pygame.image.load("assets/you_ship_v2.png")
+
+    def draw(self, playerName):
+        self.surface.blit(self.image, (self.x, self.y))
+
+    def setPosition(self, pos):
+        self.x = pos[0] - self.width / 2
+
+    def fire(self):
+        self.bullets.append(Projectiles.Bullet(self.x + self.width / 2, self.y,
+                                               self.pygame, self.surface,
+                                               self.bulletSpeed, self.bulletImage))
+        a = self.pygame.mixer.Sound(self.soundEffect)
+        a.set_volume(0.2)
+        a.play
+
+    def drawBullets(self):
+        for b in self.bullets:
+            b.move()
+            b.draw()
+
+    def registerHit(self):
+        self.health -= 1
+
+    def checkForHit(self, thingToCheckAgainst):
+        bulletsToRemove = []
+
+        for idx, b in enumerate(self.bullets):
+            if b.x > thingToCheckAgainst.x and b.x < thingToCheckAgainst.x + thingToCheckAgainst.width:
+                if b.y > thingToCheckAgainst.y and b.y < thingToCheckAgainst.y + thingToCheckAgainst.height:
+                    thingToCheckAgainst.registerHit()
+                    bulletsToRemove.append(idx)
+
+        for usedBullet in bulletsToRemove:
+            del self.bullets[usedBullet]
+
+        if thingToCheckAgainst.health <= 0:
+            return True
+
+    def __init__(self, x, y, pygame, surface, playerName):
+        self.x = x
+        self.y = y
+        self.pygame = pygame
+        self.surface = surface
+        self.playerName = playerName
+        self.loadImages(playerName)
+
+        dimensions = self.image.get_rect().size
+        self.width = dimensions[0]
+        self.height = dimensions[1]
+
+        self.x -= self.width / 2
+        self.y -= self.height + 10
+
+class Enemy(Player):
+
+    x = 0
+    y = 0
+    firing = False
+    image = None
+    soundEffect = "sounds/enemy_laser.wav"
+    bulletImage = "assets/them_pellet.png"
+    bulletSpeed = 10
+    speed = 2
+
+    def move(self):
+        self.y += self.speed
+
+    def tryToFire(self):
+        shouldFire = random.random()
+
+        if shouldFire <= 0.01:
+            self.fire()
+
+    def loadImages(self):
+        self.image = self.pygame.image.load("assets/them_ship.png")
+
+    def __init__(self, x, y, pygame, surface, health):
+        self.x = x
+        self.y = y
+        self.pygame = pygame
+        self.surface = surface
+        self.loadImages()
+        self.bullets = []
+        self.health = health
+
+        dimensions = self.image.get_rect().size
+        self.width = dimensions[0]
+        self.height = dimensions[1]
+
+        self.x -= self.width / 2
+ 
